2b747af11610512c7ebede2f5bf0578812b92968
Santu Yadav <santu.yadav2503@gmail.com>
2025-12-18 12:56:08 +0530

    infine-scrolling initial version done

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,60 @@
 
 import { useFetch } from "./useFetch";
 import Follower from "./Follower";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 function App() {
   const [page, setPage] = useState(1);
   const followerRef = useRef();
+  useCallback();
   console.log("page--02 ### :", page);
 
-  const { loading, data } = useFetch(9, page);
+  const { loading, data, hasMore } = useFetch(9, page);
 
-  const refFunction = (node) => {
-    if (followerRef.current) {
-      followerRef.current.disconnect();
-    }
-
-    followerRef.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setPage((prevPage) => prevPage + 1);
+  const refFunction = useCallback(
+    (node) => {
+      if (!node) {
+        if (followerRef.current) {
+          followerRef.current.disconnect();
+        }
+        return;
       }
-    });
 
-    if (node) followerRef.current.observe(node);
-  };
+      if (followerRef.current) {
+        followerRef.current.disconnect();
+      }
+
+      followerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && !loading && hasMore) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+
+      followerRef.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   return (
     <main>
-      {loading ? (
-        <h3>Loading...</h3>
-      ) : (
-        <section className="followers">
-          <div className="container">
-            {/* {console.log("followers @@ :", followers)} */}
-            {data &&
-              data.map((follower, index, array) => {
-                return (
-                  <Follower
-                    refFunction={refFunction}
-                    key={follower.id}
-                    follower={follower}
-                    index={index}
-                    array={array}
-                  />
-                );
-              })}
-          </div>
-        </section>
-      )}
+      <section className="followers">
+        <div className="container">
+          {/* {console.log("followers @@ :", followers)} */}
+          {data &&
+            data.map((follower, index, array) => {
+              return (
+                <Follower
+                  refFunction={refFunction}
+                  key={follower.id}
+                  follower={follower}
+                  index={index}
+                  array={array}
+                />
+              );
+            })}
+        </div>
+        {loading && <h4>loading...</h4>}
+      </section>
     </main>
   );
 }
--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -4,34 +4,55 @@ import { useState, useEffect } from "react";
 // const url =
 //   "https://api.github.com/users/octocat/followers?per_page=${limit}&page=${page}";
 
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-
+// const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 export const useFetch = (limit, page) => {
   console.log("page ### :", page);
   console.log("limit ### :", limit);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   const getProducts = async () => {
-    const response = await fetch(
-      `https://api.github.com/users/octocat/followers?per_page=${limit}&page=${page}`,
-      {
-        headers: {
-          Authorization: `Bearer ${GITHUB_TOKEN}`,
+    if (!hasMore) {
+      setLoading(false);
+      return;
+    }
 
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      }
+    const response = await fetch(
+      `https://api.github.com/users/octocat/followers?per_page=${limit}&page=${page}`
+      // {
+      //   headers: {
+      //     Authorization: `Bearer ${GITHUB_TOKEN}`,
+
+      //     "X-GitHub-Api-Version": "2022-11-28",
+      //   },
+      // }
     );
-    const data = await response.json();
-    console.log("data @@@ :", data);
-    setData((prevData) => [...prevData, ...data]);
+    const newData = await response.json();
+    if (newData.length === 0) {
+      setHasMore(false);
+    }
+    // setData((prevData) => [...prevData, ...data]);
+    setData((prevData) => {
+      const existingDataMap = new Map(prevData.map((item) => [item.id, item]));
+
+      const uniqueNewData = newData.filter(
+        (item) => !existingDataMap.has(item.id)
+      );
+
+      return [...prevData, ...uniqueNewData];
+    });
     setLoading(false);
   };
 
-  useEffect(() => {
-    getProducts();
-  }, [page, limit]);
+  console.log("data @@@ :", data);
 
-  return { loading, data };
+  useEffect(() => {
+    if (hasMore) {
+      setLoading(true);
+      getProducts();
+    }
+  }, [page, limit, hasMore]);
+
+  return { loading, data, hasMore };
 };
